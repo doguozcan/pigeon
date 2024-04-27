c4411443d4b02f10ebc871aeac04a527449d56ed
Doğu Özcan <76822280+doguozcan@users.noreply.github.com>
2024-04-27 16:48:18 +0300

    Initiliaze setting router & creating pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Profile from "./pages/Profile"
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="profile" element={<Profile />} />
+      </Routes>
+    </Router>
+  )
 }
