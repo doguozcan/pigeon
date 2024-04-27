b25b49d5e45cb29b9abe8a5de03583f570fc62cc
Doğu Özcan <76822280+doguozcan@users.noreply.github.com>
2024-04-27 17:39:19 +0300

    Initialize implement navbar UI & functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,25 @@
+import { Link } from "react-router-dom"
+import { FaHome } from "react-icons/fa"
+import { FaUser } from "react-icons/fa"
+
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <div className="flex flex-col gap-5 text-sm sm:text-xl text-sky-50 px-5">
+      <Link
+        to="/"
+        className="flex items-center p-5 gap-5 rounded-xl bg-sky-500 hover:bg-sky-700 active:bg-sky-900 transition duration-300"
+      >
+        <FaHome />
+        Home
+      </Link>
+      <Link
+        to="/profile"
+        className="flex items-center p-5 gap-5 rounded-xl bg-sky-500 hover:bg-sky-700 active:bg-sky-900 transition duration-300"
+      >
+        <FaUser />
+        Profile
+      </Link>
+    </div>
+  )
 }
 export default Navbar
