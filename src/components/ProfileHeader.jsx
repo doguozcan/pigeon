b36876d948d12032c49fd870915d33cb807b5918
Doğu Özcan <76822280+doguozcan@users.noreply.github.com>
2024-04-29 15:49:57 +0300

    Implement fetching profile name and avatar url values and then display them dynamically

--- src/components/ProfileHeader.jsx
+++ src/components/ProfileHeader.jsx
@@ -1,4 +1,4 @@
-const ProfileHeader = () => {
+const ProfileHeader = ({ avatar, profileName }) => {
   return (
     <div className="mx-2 relative mb-20">
       <img
@@ -6,10 +6,10 @@ const ProfileHeader = () => {
         className="w-full h-48 object-fill rounded-xl"
       />
       <img
-        src="https://images.unsplash.com/photo-1595347097560-69238724e7bd?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src={avatar}
         className="w-48 h-48 rounded-full object-cover absolute top-24 left-6"
       />
-      <p className="text-white text-xl ml-64">John Doe</p>
+      <p className="text-white text-xl ml-64">{profileName}</p>
     </div>
   )
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,15 +1,40 @@
 import Navbar from "../components/Navbar"
 import ProfileHeader from "../components/ProfileHeader"
 import Post from "../components/Post"
+import { useAuth } from "../hooks/useAuth"
+import { useEffect, useState } from "react"
+import { supabase } from "../api/supabaseClient"
 
 const Profile = () => {
+  const { user } = useAuth()
+  const [profileName, setProfileName] = useState("")
+  const [profileAvatar, setProfileAvatar] = useState("")
+
+  const fetchProfile = async () => {
+    const { data, error } = await supabase
+      .from("profiles")
+      .select()
+      .eq("id", user.id)
+      .single()
+
+    if (data) {
+      setProfileName(data.name)
+      setProfileAvatar(data.avatar)
+      console.log(data)
+    }
+  }
+
+  useEffect(() => {
+    fetchProfile()
+  }, [])
+
   return (
     <div className="flex m-5 max-w-5xl mx-auto gap-5">
       <div className="w-1/3">
         <Navbar />
       </div>
       <div className="w-2/3">
-        <ProfileHeader />
+        <ProfileHeader profileName={profileName} avatar={profileAvatar} />
         <Post />
       </div>
     </div>
