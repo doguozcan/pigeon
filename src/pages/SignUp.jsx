7deef95ad17aee4ebc4e8cbfbb2316b7ff1686c2
Doğu Özcan <76822280+doguozcan@users.noreply.github.com>
2024-04-29 14:53:25 +0300

    Implement sign up functionality with Supabase

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,8 +1,32 @@
 import { Link, Navigate } from "react-router-dom"
 import { useAuth } from "../hooks/useAuth"
+import { useState } from "react"
+import { supabase } from "../api/supabaseClient"
 
 const SignUp = () => {
   const { session } = useAuth()
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  async function signUpNewUser(e) {
+    e.preventDefault()
+
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+    })
+
+    if (data) {
+      alert("check your mailbox")
+      setEmail("")
+      setPassword("")
+    }
+
+    if (error) {
+      alert(error.error_description || error.message)
+    }
+  }
 
   return session ? (
     <Navigate to="/" />
@@ -18,15 +42,25 @@ const SignUp = () => {
       <form className="flex flex-col gap-2 items-center w-full mb-12">
         <label htmlFor="email">email</label>
         <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           id="email"
           className="outline-none bg-sky-900 p-2 rounded-xl w-full max-w-sm"
         />
         <label htmlFor="password">password</label>
         <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           id="password"
           className="outline-none bg-sky-900 p-2 rounded-xl w-full max-w-sm"
         />
-        <button className="bg-sky-900 px-2 py-1 rounded-xl">submit</button>
+        <button
+          className="bg-sky-900 px-2 py-1 rounded-xl"
+          onClick={signUpNewUser}
+        >
+          submit
+        </button>
       </form>
       <p>already have an account?</p>
       <Link
