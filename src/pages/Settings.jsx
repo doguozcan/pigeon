b63d994bb05ed64b3157f3767f991cf3b71a2977
Doğu Özcan <76822280+doguozcan@users.noreply.github.com>
2024-05-02 09:48:11 +0300

    Implement update name functionality on settings page

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,13 +1,61 @@
+import { useState, useEffect } from "react"
 import Navbar from "../components/Navbar"
+import { useAuth } from "../hooks/useAuth"
+import { supabase } from "../api/supabaseClient"
 
 const Settings = () => {
+  const [name, setName] = useState("")
+  const { user } = useAuth()
+
+  const fetchProfile = async () => {
+    const { data, error } = await supabase
+      .from("profiles")
+      .select()
+      .eq("id", user.id)
+      .single()
+
+    if (data) {
+      setName(data.name)
+    }
+
+    if (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchProfile()
+  }, [])
+
+  const handleChangeName = async () => {
+    const { error } = await supabase
+      .from("profiles")
+      .update({ name })
+      .eq("id", user.id)
+
+    if (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className="flex m-5 max-w-5xl mx-auto gap-5">
       <div className="w-1/3">
         <Navbar />
       </div>
       <div className="w-2/3">
-        <div className="bg-sky-100 p-5 rounded-xl mx-2">settings</div>
+        <div className="bg-sky-100 p-5 rounded-xl mx-2">
+          <div className="flex flex-col gap-2">
+            <label>your name</label>
+            <input value={name} onChange={(e) => setName(e.target.value)} />
+            <button
+              className="bg-sky-900 text-sky-50 rounded-xl p-2"
+              onClick={handleChangeName}
+            >
+              change
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   )
