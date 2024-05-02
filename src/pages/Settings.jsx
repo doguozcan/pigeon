b8b784b8ffa8b7da15cc447db39e328eae194397
Doğu Özcan <76822280+doguozcan@users.noreply.github.com>
2024-05-02 09:38:37 +0300

    Implement settings page's layout

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,4 +1,15 @@
+import Navbar from "../components/Navbar"
+
 const Settings = () => {
-  return <div>Settings</div>
+  return (
+    <div className="flex m-5 max-w-5xl mx-auto gap-5">
+      <div className="w-1/3">
+        <Navbar />
+      </div>
+      <div className="w-2/3">
+        <div className="bg-sky-100 p-5 rounded-xl mx-2">settings</div>
+      </div>
+    </div>
+  )
 }
 export default Settings
